1f4a8c01df3ef2c7c61553890dba0d66c3f6e9be
motoya <motoyao0118@gmail.com>
2022-06-30 14:56:11 +0900

    Create DB

--- app/controllers/clones_controller.rb
+++ app/controllers/clones_controller.rb
@@ -1,2 +1,21 @@
 class ClonesController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destoroy
+  end
+
 end
